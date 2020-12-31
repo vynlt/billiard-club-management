3a91ca09f6022c7a81352480df0f0e2f8849a2e6
vynlt <vynlt61811@gmail.com>
2020-12-31 14:27:51 +0700

    Add Menu Panel

--- src/app/views/home-page/home-page.module.ts
+++ src/app/views/home-page/home-page.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { NavigationBarComponent } from '../../components/navigation-bar/navigation-bar.component';
 import { PaymentPanelComponent } from '../../components/payment-panel/payment-panel.component';
+import { MenuPanelComponent } from '../../components/menu-panel/menu-panel.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
 
 import { HoverClassDirective } from '../../directives/hover-class.directive';
 
 @NgModule({
-  declarations: [HomePageComponent, NavigationBarComponent, PaymentPanelComponent, HoverClassDirective],
+  declarations: [HomePageComponent,
+     NavigationBarComponent, 
+     PaymentPanelComponent, 
+     MenuPanelComponent,
+     HoverClassDirective],
   imports: [
     CommonModule,
     HomePageRoutingModule
